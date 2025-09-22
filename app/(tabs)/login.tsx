4992fe0f887a0296f17cb40324bf9686f566079b
Josekariz <sejokarizz@gmail.com>
2025-09-22 17:29:50 +0300

    create tabs with optional icon rendering

---
+++ app/(tabs)/login.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+
+
+export default function LoginScreen() {
+  return (
+    <View>
+        <Text> Welcome to the Login Screen</Text>
+    </View>
+  )
+}
